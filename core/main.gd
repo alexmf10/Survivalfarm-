4037f79eb9ca3da1ab5b127be2e98a53e8298c2d
alexmf10 <fmartin.alex10@gmail.com>
2026-04-11 20:50:15 +0200

    Estructura base proyecto
    Estructura del proyecto base con el main (boostrap), el bus global y el gestor de servicios globales

---
+++ core/main.gd
@@ -0,0 +1,8 @@
+## Escena raíz Bootstrap (punto de entrada de la aplicación al iniciar).
+## Aquí se registrarán los servicios en el ServiceLocator.
+extends Node
+
+
+func _ready() -> void:
+	# Los servicios se registrarán aquí.
+	pass
